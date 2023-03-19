fd1f0d6f8a2c18400fa7ab71e90ee613a6306b32
ChinenKatsuki <67732612+ChinenKatsuki@users.noreply.github.com>
2023-03-19 21:16:41 +0900

    バリデーション追加

--- sql_app/cruds/rakumart_purchase_order.py
+++ sql_app/cruds/rakumart_purchase_order.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
+from pydantic import ValidationError
 import sys
 import os
 sys.path.append(os.path.abspath(".."))
@@ -11,19 +12,22 @@ def get_purchase_order(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.RakumartOrderAllCost).filter(models.RakumartOrderAllCost.is_deleted == 'False').offset(skip).limit(limit).all()
 
 def create_purchase_order(db: Session, purchase_order: PurchaseOrderCreate):
-    db_purchase_order = models.RakumartOrderAllCost(
-        purchase_order_name=purchase_order.purchase_order_name,
-        jpy_equivalent=purchase_order.jpy_equivalent,
-        domestic_postage=purchase_order.domestic_postage,
-        international_postage=purchase_order.international_postage,
-        commision=purchase_order.commision,
-        tariff=purchase_order.tariff,
-        order_product_total_price=purchase_order.order_product_total_price
-    )
-    db.add(db_purchase_order)
-    db.commit()
-    db.refresh(db_purchase_order)
-    return db_purchase_order
+    try:
+        db_purchase_order = models.RakumartOrderAllCost(
+            purchase_order_name=purchase_order.purchase_order_name,
+            jpy_equivalent=purchase_order.jpy_equivalent,
+            domestic_postage=purchase_order.domestic_postage,
+            international_postage=purchase_order.international_postage,
+            commision=purchase_order.commision,
+            tariff=purchase_order.tariff,
+            order_product_total_price=purchase_order.order_product_total_price
+        )
+        db.add(db_purchase_order)
+        db.commit()
+        db.refresh(db_purchase_order)
+        return db_purchase_order
+    except ValidationError as e:
+        print(e.json())
 
 def update_rakumart_purchase_order(db: Session, purchase_order_id: None, request: None):
     db_rakumart = db.query(models.RakumartOrderAllCost).filter(models.RakumartOrderAllCost.purchase_order_id == purchase_order_id).first()
--- sql_app/routes/schemas/rakumart_purchase_order.py
+++ sql_app/routes/schemas/rakumart_purchase_order.py
@@ -1,5 +1,5 @@
 import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 import sys
 sys.dont_write_bytecode = True
@@ -15,6 +15,42 @@ class PurchaseOrderCreate(BaseModel):
     created_at: datetime.datetime
     updated_at: datetime.datetime
     is_deleted: Optional[str] = None
+
+    @validator('purchase_order_name')
+    def purchase_order_name_str(cls, v):
+        if len(v) <= 1:
+            return v
+        raise ValueError('100文字以内で入力して下さい')
+
+    @validator('jpy_equivalent')
+    def jpy_equivalent_int(cls, v):
+        if isinstance(v, int):
+            return v
+        raise TypeError('数字を入力して下さい')
+
+    @validator('domestic_postage')
+    def domestic_postage_int(cls, v):
+        if isinstance(v, int):
+            return v
+        raise TypeError('数字を入力して下さい')
+
+    @validator('international_postage')
+    def international_postage_int(cls, v):
+        if isinstance(v, int):
+            return v
+        raise TypeError('数字を入力して下さい')
+
+    @validator('commision')
+    def commision_int(cls, v):
+        if isinstance(v, float):
+            return v
+        raise TypeError('小数点付きの数字')
+
+    @validator('order_product_total_price')
+    def order_product_total_price_int(cls, v):
+        if isinstance(v, int):
+            return v
+        raise TypeError('数字を入力して下さい')
 
 class PurchaseOrderPutRequest(BaseModel):
     purchase_order_name: str = Field(max_length=100)
